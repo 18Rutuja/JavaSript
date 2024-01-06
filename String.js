cfbd77e359ad33c0d66a44d7d8a5b4bb98e9b126
Rutuja Rathod <127573564+18Rutuja@users.noreply.github.com>
2024-01-06 12:04:31 +0000

    String methods

---
+++ String.js
@@ -0,0 +1,24 @@
+// concatination
+const name = "Rutuja";
+const reCount = 16;
+//+method
+console.log('hello my name is ' + name + ' and my repocount is ' + reCount);
+//method 2 - string interpolation
+//` ` write in side ` ` and ${}
+console.log(`hello my name is ${name} and my repocount is ${reCount}`);
+
+const gameName = new String('rutuuututututuuttu');
+console.log(gameName[2]);
+console.log(gameName.substring(2,9));
+
+// toUpperCase()
+// charAt('char')
+// .length
+// indexOf()
+//toLowerCase
+// substring(4,7)
+// .slice
+// trim() - remove white space from string
+// replace()
+// .includes("") - to find value from string
+//.split("_")  - split value from _
