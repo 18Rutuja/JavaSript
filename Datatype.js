cd63d887ce4938274c7ffda5c06de985ee139325
Rutuja Rathod <127573564+18Rutuja@users.noreply.github.com>
2024-01-06 11:35:10 +0000

    Data type summary

---
+++ Datatype.js
@@ -0,0 +1,31 @@
+//primitive
+//7 types
+// string 
+// number
+// Boolian
+// null
+// undefinrd
+// Symbol
+
+//nonprimimtive or Reference 
+//Array, Objects,  Function
+
+
+
+const Bignumber = 3247240923123432434n;
+
+//Array
+const hero = [mummy , papa, tai , bhau];
+
+//object always writtenn in {} braces
+let myObj= {
+    name: "rutu",
+    age: 19,
+}
+
+//function
+//variable function
+const myFunction = function(){
+    console.log("Rutu");
+}
+
---
+++ comparisonOperator.js
@@ -0,0 +1,50 @@
+// let age = "56243jkbv";
+// console.log(typeof age);
+// console.log(typeof age);
+
+// let num = Number(age)
+// console.log(typeof num);
+// console.log(num);
+
+// "55" => 33
+//"34fdg" => NaN
+// true =>  1; false =>0
+
+
+// let inLLogin = "";
+// let boolianNum = Boolean(inLLogin)
+// console.log(typeof boolianNum);
+// console.log(boolianNum);
+
+// 1 = true
+// 0= false
+// "rutu" = true
+
+//*****************************Operation*********************** */
+
+let val = 7;
+let negVAl = -val;
+console.log(negVAl);
+
+
+// console.log(2+2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2%2);
+
+//*****************************Comparison********************** */
+
+console.log(null >0);// false
+console.log(null >=0);// true
+console.log(null ==0); // false
+
+
+console.log(undefined >0);// false
+console.log(undefined  <0);// true
+console.log(undefined ==0); // false
+
+
+// === strict check
+// == conversion happend
