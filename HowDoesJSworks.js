b5dcf280b04abe31a79dead8ef0e331519e708f9
Rutuja Rathod <127573564+18Rutuja@users.noreply.github.com>
2024-01-08 18:34:36 +0000

    control flow

---
+++ HowDoesJSworks.js
@@ -0,0 +1,5 @@
+// *****************javascript execution context********************
+//  1) global executiion Context
+//  2) functional  executiion Context
+//  3) eval  executiion Context
+
---
+++ controlFlow.js
@@ -0,0 +1,43 @@
+//if statement
+// <, >, <=, >= , ==, !=, === , !==
+// == does not check data type
+// === use for check or knowing data type and execute properly
+
+//nested if
+
+//if else if 
+
+// && , || is logical operator
+
+// *******************Switch case****************
+//basic syntax
+// switch(key){
+//     case value:
+//         break;
+
+//         default:
+//             break;
+// }
+
+
+//*********truthy value and falsy value *****************/
+// falsy value = false, 0, -0, BigInt , "", null, undefined , NaN
+// truthy value 
+//"0" , "false", 'false', any value under ""or in string format 
+// [],{}, function(){}
+
+//****************************************** */
+
+// false == 0 gives true
+// false =='' gives true
+// 0 == ''    gives true;
+
+
+// nullish coalescing operator (??): undefined
+
+
+let val1;
+val1 = 5 ?? 10
+// val = null ?? 10 
+// val = undefined ?? 15
+console.log(val1);
